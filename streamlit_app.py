7bcbe212a2ca81821d2ea4d7d50a51a9ab6bcc15
shiestyzeus <jose4dateam@gmail.com>
2024-12-03 09:18:24 +0000

    FINAL

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,34 @@
 import streamlit as st
 from datetime import datetime
 
+# Title
 st.title("HAPPY 3 YEAR ANNIVERSARY")
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #FFB6C1;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
-if 'responses' not in st.session_state:
-    st.session_state.responses = []
+# Question 1: Date Picker
+st.header("What date are you available?")
+date = st.date_input("Choose a date", datetime.today())
 
-def save_response(response):
-    st.session_state.responses.append(response)
+if st.button("Next"):
+    # Question 2: Restaurant Choices
+    st.header("Where would you love to go out and eat?")
+    restaurants = ["Restaurant A", "Restaurant B", "Restaurant C", "Restaurant D"]
+    choice = st.selectbox("Select a restaurant", restaurants)
 
-def next_question(question, options=None, is_date=False):
-    st.write(question)
-    if is_date:
-        date = st.date_input("Choose a date", datetime.today())
+    if st.button("Next"):
+        # Question 3: Dessert Choices
+        st.header("Where do you want to get dessert?")
+        dessert_choices = ["Ice Cream", "Boba", "Bread/Treats", "Dick"]
+        dessert = st.selectbox("Select a dessert", dessert_choices)
+
         if st.button("Next"):
-            save_response(date)
-            return True
-    else:
-        choice = st.selectbox("Select an option", options)
-        if st.button("Next"):
-            save_response(choice)
-            return True
-    return False
+            # Question 4: Spend the Day
+            st.header("Where would you like to spend the rest of the day?")
+            day_choices = ["My House", "Open one of your Christmas gifts", "Take a walk"]
+            day_choice = st.selectbox("Select an option", day_choices)
 
-if next_question("What date are you available?", is_date=True):
-    if next_question("Where would you love to go out and eat?", 
-                     options=["Restaurant A", "Restaurant B", "Restaurant C", "Restaurant D"]):
-        if next_question("Where do you want to get dessert?", 
-                         options=["Ice Cream", "Boba", "Bread/Treats", "Dick"]):
-            if next_question("Where would you like to spend the rest of the day?", 
-                             options=["My House", "Open One of Your Christmas Gifts", "Take a Walk"]):
-                st.markdown(
-                    <h2 style='text-align: center;'>I LOVE YOU!</h2>
-                    <h3 style='text-align: center;'>Thanks for helping me make your day better!</h3>
-                    <div style='text-align: center;'>
-                        <img src='https://example.com/flowers.png' width='300'/>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
+            if st.button("Finish"):
+                # Thank You Message
+                st.header("I LOVE YOU!")
+                st.write("Thanks for helping me make your day better!")
+                st.markdown("<h1 style='text-align: center; color: pink;'>🌸🌸🌸</h1>", unsafe_allow_html=True)
+                st.markdown("<h1 style='text-align: center; color: red;'>❤️❤️❤️</h1>", unsafe_allow_html=True)
