ea451e75b11c43d98ecd797454ca0b572c85ae11
shiestyzeus <jose4dateam@gmail.com>
2024-12-03 09:16:15 +0000

    OFFICAL

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,51 @@
 import streamlit as st
 from datetime import datetime
-import requests
 
-# Function to display the GIF
-def display_gif(gif_url):
-    st.image(gif_url, use_column_width=True)
+st.title("HAPPY 3 YEAR ANNIVERSARY")
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #FFB6C1;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-# Page 1: Availability Question
-st.title("3 Year Anniversary Questionnaire")
-st.header("Question #1")
-st.write("Are you available on...")
+if 'responses' not in st.session_state:
+    st.session_state.responses = []
 
-# Calendar for date selection
-date = st.date_input("Select a date:", datetime.now())
+def save_response(response):
+    st.session_state.responses.append(response)
 
-# Display the GIF in the middle
-gif_url_1 = "https://media.tenor.com/images/1c1e1c1e1c1e1c1e1c1e1c1e1c1e1c1e/tenor.gif"
-display_gif(gif_url_1)
+def next_question(question, options=None, is_date=False):
+    st.write(question)
+    if is_date:
+        date = st.date_input("Choose a date", datetime.today())
+        if st.button("Next"):
+            save_response(date)
+            return True
+    else:
+        choice = st.selectbox("Select an option", options)
+        if st.button("Next"):
+            save_response(choice)
+            return True
+    return False
 
-# Next button to proceed to the next question
-if st.button("Next"):
-    st.session_state.page = 2
-
-# Page 2: Thank You Message
-if 'page' in st.session_state and st.session_state.page == 2:
-    st.header("Thank You!")
-    gif_url_2 = "https://media.tenor.com/images/2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d2d/tenor.gif"
-    display_gif(gif_url_2)
-    
-    if st.button("Next"):
-        st.write("Looking forward to our special day!")
+if next_question("What date are you available?", is_date=True):
+    if next_question("Where would you love to go out and eat?", 
+                     options=["Restaurant A", "Restaurant B", "Restaurant C", "Restaurant D"]):
+        if next_question("Where do you want to get dessert?", 
+                         options=["Ice Cream", "Boba", "Bread/Treats", "Dick"]):
+            if next_question("Where would you like to spend the rest of the day?", 
+                             options=["My House", "Open One of Your Christmas Gifts", "Take a Walk"]):
+                st.markdown(
+                    """
+                    <h2 style='text-align: center;'>I LOVE YOU!</h2>
+                    <h3 style='text-align: center;'>Thanks for helping me make your day better!</h3>
+                    <div style='text-align: center;'>
+                        <img src='https://example.com/flowers.png' width='300'/>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
